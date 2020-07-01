af58f968db13558ea4cc7b99210eba55aec81342
Meerxn <fardeenfmeeran@gmail.com>
2020-07-01 22:07:06 +0400

    skeleton methods added

--- main/WannaBet/views.py
+++ main/WannaBet/views.py
@@ -7,3 +7,33 @@ def index(request):
 
 def login(request):
     return render(request, 'WannaBet/login.html')
+  
+
+# Create your views here.
+
+# This method is responsible  for creating a bet 
+# Need to know how to push a new bet to the back end 
+def createBet(request,code):
+    return
+
+
+
+#This method is responsible for joining a bet using a code from generate code 
+def joinBet(request,code):   
+    return 
+
+
+#This method is responsible for creating the code for joining a bet or creating a bet 
+def createCode(request):  
+    return  
+
+#follow another user method need to know how to import another user (graph algo model maybe needed )
+def follow(request): 
+    return 
+
+
+
+# def login (request)
+# dont know if this needs a method     
+
+
